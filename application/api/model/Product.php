3eea69327a9f060548abdb7fb041af3688666c43
DingBaiJiao <dbj_1991@163.com>
2018-11-02 10:36:04 +0800

    13123

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -12,4 +12,8 @@ namespace app\api\model;
 class Product extends BaseModel
 {
     protected $hidden = ['delete_time', 'create_time', 'update_time', 'pivot', 'main_img_id', 'from', 'category_id'];
+
+    public function getMainImgUrlAttr($value, $data) {
+        return $this->prefixImgUrl($value, $data);
+    }
 }
--- application/route.php
+++ application/route.php
@@ -28,4 +28,4 @@ Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
 
 Route::get('api/:version/theme', 'api/:version.Theme/getSimpleList');
 
-Route::get('api/:version/theme/:id', 'api/:version.Theme/getComplexsOne');
+Route::get('api/:version/theme/:id', 'api/:version.Theme/getComplexOne');
